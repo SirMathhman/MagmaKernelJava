2996fa6448847f65e40cea0bdd6c9f294d346bb2
SirMathhman <SirMathhman@gmail.com>
2020-02-12 15:57:17 -0500

    Clean StructUnitTest

--- src/test/java/com/meti/node/struct/StructUnitTest.java
+++ src/test/java/com/meti/node/struct/StructUnitTest.java
@@ -1,20 +1,12 @@
-package com.meti;
+package com.meti.node.struct;
 
-import com.meti.exception.ParseException;
+import com.meti.Cache;
+import com.meti.Compiler;
+import com.meti.core.task.MagmaCompiler;
 import com.meti.node.Node;
-import com.meti.node.declare.DeclareParser;
-import com.meti.node.declare.VariableParser;
-import com.meti.node.declare.VariableResolver;
-import com.meti.node.primitive.ints.IntParser;
-import com.meti.node.primitive.ints.IntResolver;
-import com.meti.node.struct.ReturnParser;
-import com.meti.node.struct.StructUnit;
 import com.meti.parse.Declarations;
 import com.meti.parse.TreeDeclarations;
 import com.meti.util.CollectionCache;
-import com.meti.util.ParentParser;
-import com.meti.util.ParentResolver;
-import com.meti.util.UnitCompiler;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -23,31 +15,31 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class StructUnitTest {
     private final Cache cache = new CollectionCache();
-    private Compiler compiler;
+    private Compiler compiler = null;
 
     @Test
     void innerDeclaraion() {
         compiler.parse("val empty =: {\n" +
-                       "	val x = 10;\n" +
-                       "	val y = x;\n" +
-                       "}\n");
+                "	val x = 10;\n" +
+                "	val y = x;\n" +
+                "}\n");
         assertEquals("int _exitCode=0;" +
-                     "void *_throw=NULL;" +
-                     "void empty(){" +
-                     "int x=10;" +
-                     "int y=x;}" +
-                     "int main(){" +
-                     "return _exitCode;}", cache.render());
+                "void *_throw=NULL;" +
+                "void empty(){" +
+                "int x=10;" +
+                "int y=x;}" +
+                "int main(){" +
+                "return _exitCode;}", cache.render());
     }
 
     @Test
-    void empty() throws ParseException {
+    void empty() {
         compiler.parse("val empty =: {}");
         assertEquals("int _exitCode=0;void *_throw=NULL;void empty(){}int main(){return _exitCode;}", cache.render());
     }
 
     @Test
-    void parseComplete() throws ParseException {
+    void parseComplete() {
         Node node = compiler.parse("val complete = [Int value] => Int : {return value;}");
         assertTrue(node.render().isBlank());
         assertEquals("int _exitCode=0;void *_throw=NULL;int complete(int value){return value;}int main(){return _exitCode;}",
@@ -57,20 +49,7 @@ class StructUnitTest {
     @BeforeEach
     void setUp() {
         Declarations declarations = new TreeDeclarations();
-        Unit unit = new StructUnit(declarations, cache);
-        Parser parser = new ParentParser(
-                unit,
-                new DeclareParser(declarations),
-                new ReturnParser(declarations),
-                new IntParser(),
-                new VariableParser(declarations)
-        );
-        Resolver resolver = new ParentResolver(
-                unit,
-                new IntResolver(),
-                new VariableResolver(declarations)
-        );
-        compiler = new UnitCompiler(parser, resolver);
+        compiler = new MagmaCompiler(cache, declarations);
     }
 
     @Test
