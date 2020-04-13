b38d0d5a457539c44a14c1f22c50da240a3f061b
SirMathhman <sirmathhman@gmail.com>
2020-04-13 19:43:43 -0400

    Divide Main

--- src/main/java/com/meti/Main.java
+++ src/main/java/com/meti/Main.java
@@ -4,11 +4,14 @@ import com.meti.data.Cache;
 import com.meti.data.ListCache;
 import com.meti.parse.Node;
 
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public final class Main {
+	private static final Path MAIN = Paths.get("main.magma");
+	private static final Path OUT = Paths.get("main.c");
 	private static final Cache CACHE = new ListCache();
 	private static final Compiler COMPILER = new MagmaCompiler();
 
@@ -17,27 +20,46 @@ public final class Main {
 
 	public static void main(String[] args) {
 		try {
-			Path main = Paths.get("main.magma");
-			if (!Files.exists(main)) Files.createFile(main);
-			String collect = String.join("", Files.readAllLines(main));
-			String toParser = "val main : () => Int = {" + collect + "}";
-			Node node = COMPILER.parse(toParser);
-			String result = node.render(CACHE);
-			Path mainOut = Paths.get("main.c");
-			if (!Files.exists(mainOut)) Files.createFile(mainOut);
-			Files.writeString(mainOut, CACHE.render() + result);
+			String main = readInput();
+			String result = process(main);
+			writeOutput(result);
 		} catch (Exception e) {
-			Throwable t = e;
-			StringBuilder builder = new StringBuilder();
-			do {
-				String message = t.getMessage();
-				builder.append(message)
-						.append(" ")
-						.append(t.getStackTrace()[0])
-						.append("\n");
-				t = t.getCause();
-			} while (null != t);
-			System.out.println(builder);
+			handleException(e);
 		}
+	}
+
+	private static String readInput() throws IOException {
+		Path input = ensure(MAIN);
+		return String.join("", Files.readAllLines(input));
+	}
+
+	private static String process(String main) {
+		String mainEncapsulated = "val main : () => Int = {" + main + "}";
+		Node node = COMPILER.parse(mainEncapsulated);
+		return node.render(CACHE);
+	}
+
+	private static void writeOutput(String result) throws IOException {
+		Path output = ensure(OUT);
+		Files.writeString(output, CACHE.render() + result);
+	}
+
+	private static void handleException(Exception e) {
+		Throwable t = e;
+		StringBuilder builder = new StringBuilder();
+		do {
+			String message = t.getMessage();
+			builder.append(message)
+					.append(" ")
+					.append(t.getStackTrace()[0])
+					.append("\n");
+			t = t.getCause();
+		} while (null != t);
+		System.out.println(builder);
+	}
+
+	private static Path ensure(Path path) throws IOException {
+		if (!Files.exists(path)) Files.createFile(path);
+		return path;
 	}
 }
