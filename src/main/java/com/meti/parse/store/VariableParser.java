031777148ca69628b915bfa7d15d564f46506062
SirMathhman <sirmathhman@gmail.com>
2020-04-14 12:16:06 -0400

    Remove weird pass-through in VariableParser

--- src/main/java/com/meti/parse/store/VariableParser.java
+++ src/main/java/com/meti/parse/store/VariableParser.java
@@ -2,6 +2,7 @@ package com.meti.parse.store;
 
 import com.google.inject.Inject;
 import com.meti.Compiler;
+import com.meti.data.Scope;
 import com.meti.data.Stack;
 import com.meti.parse.Node;
 import com.meti.parse.Parser;
@@ -21,14 +22,13 @@ public class VariableParser implements Parser {
 	public Optional<Node> parse(String content, Compiler compiler) {
 		int period = content.indexOf('.');
 		if (-1 == period) {
-			return Optional.of(new VariableNode(content));
-	/*		if (stack.hasParameter(content)) {
+			if (stack.hasParameter(content)) {
 				return Optional.of(new VariableNode(content));
 			}
 			Optional<Node> node = stack.search(content)
 					.map(Scope::name)
 					.map(VariableNode::new);
-			return node;*/
+			return node;
 		} else {
 			String before = content.substring(0, period);
 			String after = content.substring(period + 1);
